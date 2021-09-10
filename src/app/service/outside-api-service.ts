48bfe20bfd2ceb1d72af17080cf7d953eeb61e2f
yuliang <yu.liang@freelog.com>
2021-09-10 15:39:24 +0800

    1.提交代码

--- src/app/service/outside-api-service.ts
+++ src/app/service/outside-api-service.ts
@@ -39,7 +39,6 @@ export class OutsideApiService implements IOutsideApiService {
                 subjectType, subjectName, contractName, eventId, password
             }
         }).catch(error => {
-            console.log(JSON.stringify(error), error.data);
             throw new ApiInvokingError(error.message, {
                 code: error.data?.apiInvokingAttachData?.code ?? 'E1999'
             });
